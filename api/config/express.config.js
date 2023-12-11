45bcd3f95adf4de93f8ef64c20790af2c03a76b0
sandeshnilaskhatiwada <sandeshnilaskhatwada@gmail.com>
2023-12-11 08:18:56 +0545

    feat: search function

--- api/config/express.config.js
+++ api/config/express.config.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const { consola } = require("consola");
+const path = require("path");
 
 const app = express();
-const BASE_URL = '/api/v1'
+const BASE_URL = "/api/v1";
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "..", "public/uploads")));
 
-app.use(`${BASE_URL}/product`, require('../controller/product.controller'));
+app.use(`${BASE_URL}/product`, require("../controller/product.controller"));
 
 app.use((error, req, res, next) => {
   const code = error.code || 500;
