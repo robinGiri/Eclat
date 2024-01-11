1c2ec57e46de870afdd2040ad946acd91e906f11
sandeshnilaskhatiwada <sandeshnilaskhatwada@gmail.com>
2024-01-11 09:48:05 +0545

    "Error solved and payment integrated"

--- api/middleware/validation.middleware.js
+++ api/middleware/validation.middleware.js
@@ -3,6 +3,7 @@ const { z } = require("zod");
 const validatedRequest = (schema) => {
   return (req, res, next) => {
     try {
+      console.log(req.body);
       let data = req.body;
       let validated = schema.parse(data);
       next();
