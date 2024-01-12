59e7cc6fba6a4c5b34debc00483d7204420665df
saugatBhupal <69229764+saugatBhupal@users.noreply.github.com>
2024-01-12 21:29:05 +0545

    Saugat/wishlist (#65)
    * chore: wishlist routes and services created
    
    * chore : wishlist frontend implemented
    
    ---------
    
    Co-authored-by: Robin Giri <45058288+robinGiri@users.noreply.github.com>

--- api/config/express.config.js
+++ api/config/express.config.js
@@ -33,6 +33,7 @@ app.use(`${BASE_URL}/setting`, require("../controller/setting.controller"));
 app.use(`${BASE_URL}/home`, require("../controller/home.controller"));
 app.use(`${BASE_URL}/purchase`, require("../controller/purchase.controller"));
 app.use(`${BASE_URL}/shipping`, require("../controller/shipping.controller"));
+app.use(`${BASE_URL}/wishlist`, require("../controller/wishlist.controller"));
 app.use(`${BASE_URL}/strip-payment`, require("../controller/strip.payment"));
 app.use(`${BASE_URL}/return`, require("../controller/return.controller"));
 
