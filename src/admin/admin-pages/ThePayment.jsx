633627b92afbf17e357e3801c3f8a8cdb55ad477
sandeshnilaskhatiwada <sandeshnilaskhatwada@gmail.com>
2024-01-17 20:52:44 +0545

    some structured

--- src/admin/admin-pages/ThePayment.jsx
+++ src/admin/admin-pages/ThePayment.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import TheStripPayment from "../../components/payment/TheStripPayment";
 import ThePaypalPayment from "../../components/payment/ThePaypalPayment";
+import TheKhaltiPayment from "../../components/payment/TheKhaltiPayment";
+import Khaltitest from "../../pages/Khaltitest";
 
 function ThePayment() {
   return (
     <div className="flex mt-20">
+      <Khaltitest />
       <TheStripPayment />
       <ThePaypalPayment />
     </div>
