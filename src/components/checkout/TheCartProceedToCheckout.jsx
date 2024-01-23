2ff31f6a66ba5b4e1e249a156808ea3306b5fe27
Abhisek Thapa <112496493+abhisekthapam@users.noreply.github.com>
2024-01-23 12:12:08 +0545

    Abhisek/checkout payment (#77)
    * chore:chart data
    
    * config
    
    * "Chore: calculations table"
    
    * fix: image fixed
    
    * "fix: user image"
    
    * fix: bug fixes:
    
    * fix
    
    * feat: checkout payment
    
    ---------
    
    Co-authored-by: sandeshnilaskhatiwada <sandeshnilaskhatwada@gmail.com>
    Co-authored-by: abhisekthapam <ahisekmagar250203@gmail.com>

--- src/components/checkout/TheCartProceedToCheckout.jsx
+++ src/components/checkout/TheCartProceedToCheckout.jsx
@@ -11,37 +11,37 @@ function TheCartProceedToCheckout({ total }) {
     <div className="p-4">
       <div className="flex flex-col gap-3">
         <div>
-          <p className="text-xl font-semibold">Order Summary</p>
+          <p className="font-semibold">Order Summary</p>
         </div>
         <div className="flex justify-between">
-          <p className="text-gray-500 text-sm">Subtotal (7 items)</p>
-          <p>${total}</p>
+          <p className="text-gray-500 text-[13px]">Subtotal (7 items)</p>
+          <p className="text-gray-500">${total}</p>
         </div>
         <div className="flex justify-between">
-          <p className="text-gray-500 text-sm">Shipping Fee</p>
-          <p>$120</p>
+          <p className="text-gray-500 text-[13px]">Shipping Fee</p>
+          <p className="text-gray-500">$120</p>
         </div>
         <div className="flex justify-between">
-          <p className="text-gray-500 text-sm">Shipping Fee Discount</p>
-          <p>-$120</p>
+          <p className="text-gray-500 text-[13px]">Shipping Fee Discount</p>
+          <p className="text-gray-500">-$120</p>
         </div>
         <div className="flex justify-between">
           <input
             type="text"
             placeholder="Enter Voucher Code"
-            className="border w-[78%] p-2 focus:outline-sky-600 rounded-md"
+            className="border w-[78%] p-2 focus:outline-none rounded-md"
           />
-          <button className="bg-sky-500 p-2 px-4 text-white rounded-md">
+          <button className="text-sm  p-2 px-4 border hover:text-green-500 hover:border-green-500 rounded-md">
             APPLY
           </button>
         </div>
         <div className="flex justify-between">
-          <p className="font-semibold">Total</p>
-          <p className="text-orange-500">${total}</p>
+          <p className="font-semibold text-gray-500">Total</p>
+          <p className="text-gray-500">${total}</p>
         </div>
         <div>
           <Link to="/cart/place-order" onClick={handleCartOrderPlace}>
-          <button className="border w-full p-2 mt-4 rounded-md bg-orange-500 hover:bg-orange-600 text-white">
+          <button className="border w-full p-2 mt-4 rounded-md hover:text-green-500 hover:border-green-500 text-[13px]">
             PROCEED TO CHECKOUT
           </button>
           </Link>
