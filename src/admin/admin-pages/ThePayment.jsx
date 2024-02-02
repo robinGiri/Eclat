097f66616f628758f7d0e5fb3c224b634e8d534e
Abhisek Thapa <112496493+abhisekthapam@users.noreply.github.com>
2024-02-02 15:23:35 +0545

    fix: bug fixes (#86)
    Co-authored-by: abhisekthapam <ahisekmagar250203@gmail.com>

--- src/admin/admin-pages/ThePayment.jsx
+++ src/admin/admin-pages/ThePayment.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import { useState } from "react";
 import TheStripPayment from "../../components/payment/TheStripPayment";
-import ThePaypalPayment from "../../components/payment/ThePaypalPayment";
 import Khaltitest from "../../components/payment/KhaltiPayment";
+import ThePaymentModal from "../../components/payment/ThePaymentModal";
+import { PaypalButton } from "../../components/payment/ThePaymentButtons";
 
 function ThePayment() {
+  const [isPaypalModalOpen, setIsPaypalModalOpen] = useState(false);
+
+  const handlePaypalModalOpen = () => {
+    setIsPaypalModalOpen(true);
+  };
+
+  const handlePaypalModalClose = () => {
+    setIsPaypalModalOpen(false);
+  };
   return (
-    <div className="flex flex-col mt-20">
+    <div className="flex flex-col  m-10">
+      <div>
       <TheStripPayment />
+      </div>
+      <div className="mx-3 mt-2">
       <Khaltitest />
-      <ThePaypalPayment />
+      </div>
+      <div className="-mx-2 -mt-7">
+      <button onClick={handlePaypalModalOpen}>
+            <PaypalButton />
+          </button>
+      </div>
+      {isPaypalModalOpen && <ThePaymentModal close={handlePaypalModalClose} />}
     </div>
   );
 }
